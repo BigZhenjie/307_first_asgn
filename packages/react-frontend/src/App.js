6c80b628eb03b31cd56d4c6dfdcc8584254354f4
BigZhenjie <you@example.com>
2024-01-30 09:20:33 -0800

    updated frontend code

--- packages/react-frontend/src/App.js
+++ packages/react-frontend/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 import Table from './Table';
 import MyForm from './MyForm';
@@ -6,13 +7,37 @@ import MyForm from './MyForm';
 function App() {
   const [characters, setCharacters] = useState([]);
 
+  function fetchUsers(){
+    axios.get('http://localhost:8000/users')
+    .then(res => {
+      setCharacters(res.data.users_list);
+    })
+    .catch(err => console.log(err));
+  }
+  function postUser(user){
+    axios.post('http://localhost:8000/users', user)
+    .then(res => {
+      fetchUsers();
+    })
+    .catch(err => console.log(err));
+  }
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
   function updateList(person) {
-    setCharacters([...characters, person]);
+    postUser(person);
   }
 
   function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
-      return i !== index;
+      if (i !== index) {
+        return true;
+      }
+      else{
+        axios.delete(`http://localhost:8000/users/${character.id}`);
+        return false;
+      }
     });
     setCharacters(updated);
   }
